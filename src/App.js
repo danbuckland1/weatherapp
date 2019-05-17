ee435eac4fc9bb1bc3f3f5ded86e0da797e1e69f
danbuckland1 <buckd409@gmail.com>
2019-05-16 21:02:40 -0400

    working on css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import React from "react";
-
-import Titles from "./components/Titles";
+import React  from "react";
 import Form from "./components/Form";
 import Weather from "./components/Weather";
 
@@ -22,13 +20,14 @@ class App extends React.Component {
     const api_call = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}&units=imperial`
     );
+
     const data = await api_call.json();
     if (city) {
-      
       this.setState({
         temperature: data.main.temp,
         city: data.name,
         humidity: data.main.humidity,
+        wind: data.wind.speed,
         description: data.weather[0].description,
         error: ""
       });
@@ -37,22 +36,24 @@ class App extends React.Component {
         temperature: undefined,
         city: undefined,
         humidity: undefined,
+        wind: undefined,
         description: undefined,
         error: "Please enter a city"
       });
     }
+    console.log(data);
   };
 
   render() {
     return (
       <div>
-        <Titles />
         <Form getWeather={this.getWeather} />
         <Weather
           temperature={this.state.temperature}
           city={this.state.city}
           humidity={this.state.humidity}
           description={this.state.description}
+          wind={this.state.wind}
           error={this.state.error}
         />
       </div>
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,11 +2,12 @@ import React from "react";
 
 const Weather = props => (
     <div>
-    {props.city && <p>City: {props.city}</p>}
-    {props.temperature && <p>Temperature: {props.temperature}</p>}
-    {props.humidity && <p>Humidity: {props.humidity}</p>}
-    {props.description && <p>Conditions: {props.description}</p>}
-    {props.error && <p>{props.error}</p>}
+    {props.city && <p className= "city">{props.city}</p>}
+    {props.temperature && <p className="temp">{props.temperature}º</p>}
+    {props.humidity && <p className="humidity"> Humidity:{props.humidity}%</p>}
+    {props.wind && <p className="wind">Wind:{props.wind}mph</p>}
+    {props.description && <p className="conditions">Conditions:{props.description}</p>}
+    {props.error && <p className="error">{props.error}</p>}
   </div>
 );
 export default Weather;
